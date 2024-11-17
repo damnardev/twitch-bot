a89f39eed815f7223dc449b4a2f9cf13245adc1a
damnardev <184518315+damnardev@users.noreply.github.com>
2024-11-17 18:37:01 +0100

    feat: update Wizebot Shoutout flag

---
+++ bot-domain/src/main/java/fr/damnardev/twitch/bot/domain/service/raid/DefaultUpdateRaidConfigurationMessageService.java
@@ -0,0 +1,44 @@
+package fr.damnardev.twitch.bot.domain.service.raid;
+
+import fr.damnardev.twitch.bot.domain.DomainService;
+import fr.damnardev.twitch.bot.domain.model.event.RaidConfigurationUpdatedEvent;
+import fr.damnardev.twitch.bot.domain.model.form.UpdateRaidConfigurationForm;
+import fr.damnardev.twitch.bot.domain.port.primary.raid.UpdateRaidConfigurationService;
+import fr.damnardev.twitch.bot.domain.port.secondary.EventPublisher;
+import fr.damnardev.twitch.bot.domain.port.secondary.channel.FindChannelRepository;
+import fr.damnardev.twitch.bot.domain.port.secondary.raid.FindRaidConfigurationRepository;
+import fr.damnardev.twitch.bot.domain.port.secondary.raid.UpdateRaidConfigurationRepository;
+import fr.damnardev.twitch.bot.domain.service.DefaultTryService;
+
+@DomainService
+public class DefaultUpdateRaidConfigurationMessageService extends AbstractRaidConfigurationMessageService implements UpdateRaidConfigurationService {
+
+	private final DefaultTryService tryService;
+
+	private final UpdateRaidConfigurationRepository updateRaidConfigurationRepository;
+
+	public DefaultUpdateRaidConfigurationMessageService(FindChannelRepository findChannelRepository, FindRaidConfigurationRepository findRaidConfigurationRepository, EventPublisher eventPublisher, DefaultTryService tryService, UpdateRaidConfigurationRepository updateRaidConfigurationRepository) {
+		super(findChannelRepository, findRaidConfigurationRepository, eventPublisher);
+		this.tryService = tryService;
+		this.updateRaidConfigurationRepository = updateRaidConfigurationRepository;
+	}
+
+	@Override
+	public void process(UpdateRaidConfigurationForm form) {
+		this.tryService.doTry(this::doInternal, form);
+	}
+
+	private void doInternal(UpdateRaidConfigurationForm form) {
+		var raidConfiguration = getRaidConfiguration(form.channelName());
+		if (raidConfiguration == null) {
+			return;
+		}
+		if (form.wizebotShoutoutEnabled() != null) {
+			raidConfiguration = raidConfiguration.toBuilder().wizebotShoutoutEnabled(form.wizebotShoutoutEnabled()).build();
+		}
+		this.updateRaidConfigurationRepository.update(raidConfiguration);
+		var event = RaidConfigurationUpdatedEvent.builder().raidConfiguration(raidConfiguration).build();
+		this.eventPublisher.publish(event);
+	}
+
+}
