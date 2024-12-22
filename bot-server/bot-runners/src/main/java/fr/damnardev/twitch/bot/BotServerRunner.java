60a0c14494ff89b9e23bff598289bfd044c098e4
damnardev <184518315+damnardev@users.noreply.github.com>
2024-12-22 17:00:21 +0100

    feat: split server & client (#151)

---
+++ bot-server/bot-runners/src/main/java/fr/damnardev/twitch/bot/BotServerRunner.java
@@ -0,0 +1,17 @@
+package fr.damnardev.twitch.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
+
+@ComponentScan(basePackages = "fr.damnardev.twitch.bot", includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = DomainService.class) }, nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
+@SpringBootApplication
+public class BotServerRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotServerRunner.class, args);
+	}
+
+}
